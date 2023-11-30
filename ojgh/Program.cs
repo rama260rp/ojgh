d661ed4c08b897d157f64c208d57d49a2806fffb
ramazan260 <ramazan260@BPC06>
2023-11-30 13:12:45 +0300

    Proje dosyası ekle.

---
+++ ojgh/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ojgh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KlavyeGırılenSayi klavye = new KlavyeGırılenSayi();
+            klavye.toplaMetodu();
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+}
+public class KlavyeGırılenSayi
+{
+    public void toplaMetodu()
+    {
+
+        int sayi, toplam = 0;
+        while (true)
+        {
+            Console.Write("Sayı Gir : ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi == 0)
+            {
+                break;
+            }
+            toplam += sayi;
+        }
+        Console.WriteLine("Girdiğiniz Sayıların Toplamı: " + toplam);
+        Console.ReadKey();
+    }
+    }
+}
